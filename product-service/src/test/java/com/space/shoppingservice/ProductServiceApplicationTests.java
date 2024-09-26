9b523c840de54e370d791cfacd0921bce9951c16
gbakradze94 <Bakradzegeorge17@gmail.com>
2024-09-26 22:24:26 +0400

    Add h2 in-memory database for test profile

--- product-service/src/test/java/com/space/shoppingservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/space/shoppingservice/ProductServiceApplicationTests.java
@@ -4,19 +4,24 @@ import com.space.shoppingservice.domain.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@ActiveProfiles("test")
 class ProductServiceApplicationTests {
 
 	@Autowired
 	private WebTestClient webTestClient;
 
 	@Test
+	@Transactional
 	void whenPostRequestThenProductCreated() {
 		var expectedProduct = Product.of("1234567890", "GoPro Hero 7", "Action camera", 1500.0);
 
